5c33260cd2ce8c3e93240e37c717e371fafb57b9
FraterSKS <jackson.a.furr@gmail.com>
2023-01-23 19:17:24 -0500

    added initcards

--- Cards/Card.java
+++ Cards/Card.java
@@ -1,8 +1,12 @@
+package Cards;
+
 public class Card {
+	private String name;
 	private int attack;
 	private int defense;
 
-	public Card(int attack, int defense) {
+	public Card(String name, int attack, int defense) {
+		this.name = name;
 		this.attack = attack;
 		this.defense = defense;
 	}
---
+++ Cards/InitCards.java
@@ -0,0 +1,5 @@
+package Cards;
+
+public class InitCards {
+	private Card fireball = new Card("Fireball", 12, 5);
+}
